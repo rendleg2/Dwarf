21db0fbb1a7983406c9bdb84948b1bb7713ad9c0
rendleg2 <20136@rosmini.school.nz>
2024-10-04 15:14:43 +1300

    fix?

--- Scripts/settings.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-var Settings_var = {"renderDistance": 2, "chunkSize": 3}
-@onready var World_settings = {"seed": randi(), "NoiseMapOne" : FastNoiseLite.new(), "rng": RandomNumberGenerator.new(), "chunkLoaderOne": null}
-var chunks = {}
-func _ready():
-	World_settings["NoiseMapOne"].seed = World_settings["seed"]
-	World_settings["rng"].seed = World_settings["seed"]
-	print(World_settings["NoiseMapOne"].seed)
