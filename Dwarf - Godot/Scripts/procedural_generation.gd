23f7ec8f947d3340e94a77cbf6742bb1b041b504
rendleg2 <20136@rosmini.school.nz>
2024-10-04 15:07:17 +1300

    added table

--- Dwarf - Godot/Scripts/procedural_generation.gd
+++ Dwarf - Godot/Scripts/procedural_generation.gd
@@ -1,12 +1,26 @@
 extends Node2D
 @onready var Settings_var = Settings.Settings_var
 @onready var World_settings = Settings.World_settings
+@onready var chunks = Settings.chunks
+var chunksNumber = 0
+var var_name = null
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	print("redy world")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
 	pass
+
+func save():
+	chunksNumber += 1
+	var_name = "chunk_" + str(chunksNumber)
+	chunks[var_name] = []
+	for x in range(0, Settings_var["chunkSize"]):
+		chunks[var_name].append([{"x": x}])
+		for y in range(0, Settings_var["chunkSize"]):
+			chunks[var_name][x].append([{"block": y}])
+
+func locate():
+	var cords = World_settings["chunkLoaderOne"].global_position
+
+func generateChunks():
+	pass
+	if chunks.has("notting lol"):
+		pass
