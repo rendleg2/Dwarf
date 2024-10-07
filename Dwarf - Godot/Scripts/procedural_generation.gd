87cf806221fad39c98a3f78ee28c37ef4c8ece1a
rendleg2 <20136@rosmini.school.nz>
2024-10-07 14:47:42 +1300

    step 2 of prosedual gen conplete

--- Dwarf - Godot/Scripts/procedural_generation.gd
+++ Dwarf - Godot/Scripts/procedural_generation.gd
@@ -12,33 +12,39 @@ var var_name = null
 func _ready():
 	pass
 
-func save():
+func save(cords):
 	chunksNumber += 1
-	var_name = "chunk_" + str(chunksNumber)
+	var_name = str(cords.x) + "," + str(cords.y)
 	chunks[var_name] = []
 	for x in range(0, Settings_var["chunkSize"]):
 		chunks[var_name].append([{"x": x}])
 		for y in range(0, Settings_var["chunkSize"]):
-			chunks[var_name][x].append([{"block": y}])
+			chunks[var_name][x].append({"cords": str(x,",",y)})
+	for x in range(0, Settings_var["chunkSize"]):
+		chunks[var_name][x].remove_at(0)
 
 func locate():
 	cords = World_settings["chunkLoaderOne"].global_position
 	var cords2 = snapped(Ground.local_to_map(Vector2i(cords.x, cords.y)), Vector2i(Settings_var["chunkSize"], Settings_var["chunkSize"]))
-	var cords3 = Ground.map_to_local(Vector2i(cords2.x, cords2.y))
-	print(cords, "   ", cords2, "   ", cords3)
-	generateChunks(cords3)
+	generateChunks(cords2)
 	
-func generateChunks(cords):
-	var cords4 = Ground.local_to_map(cords)
-	print(cords4)
-	if chunks.has("notting lol"):
-		pass
-	for x in range(Settings_var["chunkSize"]):
-		for y in range(Settings_var["chunkSize"]):
-			var gen = World_settings["NoiseMapOne"].get_noise_2d(cords4.x, cords4.y)
-			var pos = Vector2i(cords4.x+x, cords4.y+y)
-			if gen < 1 and gen > -1:
-				Ground.set_cell(pos, 0, Vector2i(0, 0))
+func generateChunks(cords4):
+	if chunks.has(str(cords4.x, ",", cords4.y)):
+		print("no")
+	else:
+		print("yes")
+		save(cords4)
+		for x in range(Settings_var["chunkSize"]):
+			for y in range(Settings_var["chunkSize"]):
+				var gen = World_settings["NoiseMapOne"].get_noise_2d(cords4.x+x, cords4.y+y)
+				var pos = Vector2i(cords4.x+x, cords4.y+y)
+				if gen < 1 and gen > -1:
+					Ground.set_cell(pos, 0, Vector2i(0, 0))
+					var stats = {
+						"Block": "test",
+						"Hp": 100
+					}
+					chunks[str(cords4.x) + "," + str(cords4.y)][x][y].merge(stats)
 
 func _on_timer_timeout():
 	locate()
